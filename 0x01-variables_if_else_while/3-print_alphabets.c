7cd37d655d9dd44e9322b2f4d07a8cdf37a49b83
Marvin Osolo <osolomarvin@gmail.com>
2022-06-03 07:37:03 -0700

    2nd 1/2 + corrections

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,11 +7,12 @@
  */
 int main(void)
 {
-  char a;
-  for (a = 'a'; a <= 'z'; a++)
-    putchar(a);
-  for (a = 'A'; a <= 'Z'; a++)
-    putchar(a);
-  putchar('\n');
-  return 0;
+char a;
+ 
+for (a = 'a'; a <= 'z'; a++)
+putchar(a);
+for (a = 'A'; a <= 'Z'; a++)
+putchar(a);
+putchar('\n');
+return (0);
 }
--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -8,9 +8,10 @@
 int main(void)
 {
 char a;
- 
-for (a = 'a'; a <= 'z'; a++)
-putchar(a);
+
+for (a = 'z'; a >= 'a'; a--)
+  putchar(a);
 putchar('\n');
 return (0);
+  
 }
