a04fcfbd0b89cb40e5e1bf9f45d98d3b65bc3b15
Marvin Osolo <osolomarvin@gmail.com>
2022-06-09 04:25:20 -0700

    one to three

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: int parameter
+ * @b: int parameter
+ * Return: the result of the operation
+ */
+int mul(int a, int b)
+{
+  return (a * b);
+}
