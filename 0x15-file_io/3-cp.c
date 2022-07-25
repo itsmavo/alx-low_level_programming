63aaf054495b5c4f0e82f88eec69599cff1add9a
Marvin Osolo <osolomarvin@gmail.com>
2022-07-25 10:55:41 -0700

    Incitatous

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,7 +8,8 @@
  */
 int main (int ac, char **av)
 {
-  int fdFr, fdTo, wrt, rd;
+  int fdFr, fdTo;
+  ssize_t wrt, rd;
   char buff[1024];
 
   if (ac != 3)
@@ -16,20 +17,31 @@ int main (int ac, char **av)
       dprintf(STDERR_FILENO, "Usage: cp file_from file_to \n");
       exit(97);
     }
+  
   fdFr = open(av[1], O_RDONLY);
   if (fdFr == -1)
     {
       dprintf(STDERR_FILENO, "Error: Can't read from %s\n", av[1]);
       exit(98);
     }
-  fdTo = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+  
+  fdTo = open(av[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
   if (fdTo == -1)
     {
       dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
       exit(99);
     }
-  while ((rd = read(fdFr, buff, 1024)) > 0)
+  
+  rd = 1024;
+  while (rd == 1024)
     {
+      rd = read(fdFr, buff, 1024);
+      if (rd == -1)
+	{
+	  dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+	  exit(98);
+	}
+
       wrt = write(fdTo, buff, rd);
       if (wrt == -1)
 	{
@@ -37,19 +49,16 @@ int main (int ac, char **av)
 	  exit(99);
 	}
     }
-  if (rd == -1)
+ 
+  if (close(fdFr) == -1)
     {
-      dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-      exit(98);
-    }
-  if (close(fdFr == -1))
-    {
-      dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdFr);
+      dprintf(STDERR_FILENO, "Error: Can't close fdFr %d\n", fdFr);
       exit(100);
     }
-  if (close(fdTo == -1))
+  
+  if (close(fdTo) == -1)
       {
-	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdTo);
+	dprintf(STDERR_FILENO, "Error: Can't close fdTo %d\n", fdTo);
 	exit(100);
       }
 
